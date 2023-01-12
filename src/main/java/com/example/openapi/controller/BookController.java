8c41619af5e1a06d390764d87fc2c21a14c87b8a
yeongun <gjduddns@gmail.com>
2023-01-12 21:10:35 +0900

    feat: @Operation, @ApiResponse 코드 추가

--- src/main/java/com/example/openapi/controller/BookController.java
+++ src/main/java/com/example/openapi/controller/BookController.java
@@ -3,6 +3,11 @@ package com.example.openapi.controller;
 import com.example.openapi.entity.Book;
 import com.example.openapi.exception.BookNotFoundException;
 import com.example.openapi.repository.BookRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -16,6 +21,15 @@ public class BookController {
     @Autowired
     private BookRepository repository;
 
+    @Operation(summary = "Get a book by its id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Found the book",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Book.class))}),
+            @ApiResponse(responseCode = "400", description = "Invalid id supplied",
+                    content = @Content),
+            @ApiResponse(responseCode = "404", description = "Book not found",
+                    content = @Content)})
     @GetMapping("/{id}")
     public Book findById(@PathVariable long id) {
         return repository.findById(id)
